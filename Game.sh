c86253ae31296bf32d397519e9ba60ac59ba9b03
Jagoum-Richmond <jagoumr@gmail.com>
2024-09-30 15:25:08 +0100

    New Comit

--- Game.sh
+++ Game.sh
@@ -3,12 +3,12 @@ guessing_number() {
 	#We declare a random number and assign it to a variable called random-number
 	random_number=$((RANDOM % $1))
 	#here we use the for loop to loop , through the game
-	while [ i lt 3]; do
+	while [ i l-t 3]; do
 		# if the random number genrated by the computer and that inputed by the user is thesame
 		i = $i + 1
 		if [ $random_number -eq $2 ]; then
 			echo "You Won!"
-			break
+			break?
 		elif [ $random_number -gt $2 ]; then
 			echo "You missed! The number is greater than $2"
 			echo "Please try again: "
@@ -21,7 +21,7 @@ guessing_number() {
 			2 = $guest_number
 		fi
 	done
-	if [ $i eq 3 ]; then
+	if [ $i -gt 2 ]; then
 		echo "You Lose, the number was $random_number"
 		else 
 		echo "Pecfect one"
